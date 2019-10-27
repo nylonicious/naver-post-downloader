56d01722a99bc1e8b515cba6bba9feac1d05778e
Nylonicious <50183564+Nylonicious@users.noreply.github.com>
2019-10-27 17:11:43 +0100

    Script simplification

--- naver_post_downloader.py
+++ naver_post_downloader.py
@@ -8,46 +8,39 @@ from urllib.parse import unquote, urlparse
 import aiohttp
 
 
-class NaverDownloader:
-    def __init__(self, url):
-        self.session = None
-        asyncio.run(self.queue_downloads(url))
+async def queue_downloads(url):
+    title = urlparse(url).query.split('volumeNo=')[1].split('&')[0]
+    desired_path = Path.cwd() / title
+    desired_path.mkdir(parents=False, exist_ok=True)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'}
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(url) as response:
+            text = await response.text()
+            for item in text.split("data-linkdata=\'")[1:]:
+                linkdata = loadjson(item.split("\'>\n")[0])
+                if 'src' in linkdata:
+                    picture_url = linkdata['src']
+                    picture_id = unquote(urlparse(picture_url).path.split('/')[-1])
+                    picture_name = re.sub('[<>:\"/|?*]', ' ', picture_id).strip()
+                    picture_path = desired_path / picture_name
+                    if not desired_path.is_file():
+                        await download(session, picture_url, picture_path)
+                else:
+                    print(f"Error string does not include 'src' {linkdata}")
 
-    async def queue_downloads(self, url):
-        title = urlparse(url).query.split('volumeNo=')[1].split('&')[0]
-        desiredpath = Path.cwd() / title
-        if not desiredpath.exists():
-            desiredpath.mkdir(parents=True)
-        timeout = aiohttp.ClientTimeout(total=60)
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0'}
-        async with aiohttp.ClientSession(timeout=timeout, headers=headers) as self.session:
-            async with self.session.get(url) as response:
-                text = await response.text()
-                for i in text.split("data-linkdata=\'")[1:]:
-                    linkdata = loadjson(i.split("\'>\n")[0])
-                    if 'src' in linkdata:
-                        picture_url = linkdata['src']
-                        picture_id = unquote(urlparse(picture_url).path.split('/')[-1])
-                        picture_name = re.sub('[<>:\"/|?*]', ' ', picture_id).strip()
-                        picture_path = desiredpath / picture_name
-                        if not desiredpath.is_file():
-                            await self.download(picture_url, picture_path)
-                    else:
-                        print(f"Error string does not include 'src' {linkdata}")
 
-    async def download(self, picture_url, picture_path):
-        async with self.session.get(picture_url) as r:
-            if r.status == 200:
-                with open(picture_path, 'wb') as f:
-                    print(f'Downloading {picture_url}')
-                    f.write(await r.read())
-            else:
-                print(f'Error {r.status} while getting request for {picture_url}')
+async def download(session, picture_url, picture_path):
+    async with session.get(picture_url) as r:
+        if r.status == 200:
+            picture_path.write_bytes(await r.read())
+            print(f'Downloaded {picture_url}')
+        else:
+            print(f'Error {r.status} while getting request for {picture_url}')
 
 
 def main():
     urlinput = input('Enter post URL: ')
-    NaverDownloader(urlinput)
+    asyncio.run(queue_downloads(urlinput))
 
 
 if __name__ == '__main__':
